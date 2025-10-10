4c0a6966908e6a9a1393fbd8c8ef34a0e9437c23
Amrit Pramanik <pramanikamrit778@gmail.com>
2025-10-10 14:43:41 +0530

    Initial Commit

--- frontend/src/components/Navber.jsx
+++ frontend/src/components/Navber.jsx
@@ -12,26 +12,26 @@ const Navber = () => {
   return (
     <div className='flex  items-center justify-between py-5 mb-5 '>
       <img className='w-40 cursor-pointer' src={assets.logo} alt="" />
-      <ul className='hidden lg:flex items-start gap-10 px-12 py-2.5 text-[14px] rounded-[30px] shadow-[0_0_30px_0_rgba(0,0,0,0.1)] font-medium'>
+      <ul className='hidden lg:flex items-start gap-10 px-12 py-2.5 text-[15px] rounded-[30px] shadow-[0_0_30px_0_rgba(0,0,0,0.1)] font-medium'>
         <NavLink to={'/'}>
           <li className='py-1 '>Home</li>
-          <hr className='border-none outline-none h-0.5 bg-[#5f6fff] w-3/3 m-auto hidden'/>
+          {/* <hr className='border-none outline-none h-0.5 bg-[#5f6fff] w-3/3 m-auto hidden'/> */}
         </NavLink>
         <NavLink to={'/doctors'}>
           <li className='py-1'>Doctors</li>
-          <hr className='border-none outline-none h-0.5 bg-[#5f6fff] w-4/4 m-auto hidden'/>
+          {/* <hr className='border-none outline-none h-0.5 bg-[#5f6fff] w-4/4 m-auto hidden'/> */}
         </NavLink>
         <NavLink to={'/my-appointments'}>
           <li className='py-1'>Appointment</li>
-          <hr className='border-none outline-none h-0.5 bg-[#5f6fff] w-3/3 m-auto hidden'/>
+          {/* <hr className='border-none outline-none h-0.5 bg-[#5f6fff] w-3/3 m-auto hidden'/> */}
         </NavLink>
         <NavLink to={'/about'}>
           <li className='py-1'>About</li>
-          <hr className='border-none outline-none h-0.5 bg-[#5f6fff] w-3/3 m-auto hidden'/>
+          {/* <hr className='border-none outline-none h-0.5 bg-[#5f6fff] w-3/3 m-auto hidden'/> */}
         </NavLink>
         <NavLink to={'/contact'}>
           <li className='py-1'>Contact</li>
-          <hr className='border-none outline-none h-0.5 bg-[#5f6fff] w-3/3 m-auto hidden'/>
+          {/* <hr className='border-none outline-none h-0.5 bg-[#5f6fff] w-3/3 m-auto hidden'/> */}
         </NavLink>
       </ul>
       <div className='flex items-center gap-4'>
@@ -49,7 +49,7 @@ const Navber = () => {
               </div>
             </div> 
           : <button onClick={()=>navigate('/login')}
-            className='bg-[#3f53ff] text-white px-8 py-2 text-[16px] rounded-full cursor-pointer hidden md:block'>
+            className='primary text-white px-8 py-2 text-[16px] rounded-full cursor-pointer hidden md:block'>
               Login
             </button>
       }
